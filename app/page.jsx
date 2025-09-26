35e4a6141f615d34685dbf8c6e15429c340bdf74
febbynf <febbynurfitriyani1208@gmail.com>
2025-09-26 23:57:43 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Topbar from "@/components/Topbar";
+
+const Home = () => {
+    return (
+        <div className="w-full max-w-[1140px] mx-auto overflow-hidden bg-white">
+          <Topbar />
+            <div className="h-[4000px]">
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
